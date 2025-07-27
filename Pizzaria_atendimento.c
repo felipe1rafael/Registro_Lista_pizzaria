713c0ca9ee606c0b08cb02669ac57ca326eb3995
felipe rafael <fr2794763@gmail.com>
2025-07-27 19:34:51 -0300

    Update Pizzaria_atendimento.c

--- Pizzaria_atendimento.c
+++ Pizzaria_atendimento.c
@@ -8,43 +8,6 @@ struct Lista{
     int atendido;
 };
 struct Lista Lista[200] ;
-
-int main (){
-    int  atendimento;
-    int i =0;
-    int final =0;
-    do{
-        printf("[1] Novo pedido\n[2] Atender Pedido\n[3] Ver fila\n[4]Encerrar progama\n");
-        scanf("%d",&atendimento);
-        if(atendimento == 1){
-          printf("\n--- Novo Pedido ---\n");
-            Lista[i].pedido = i + 1;
-            adicionarNome(Lista[i].cliente);
-            adicionarSabor(Lista[i].sabor);
-            selecionarTamanho(Lista[i].tamanho);
-            Lista[i].atendido = 0;
-            printf("Pedido %d registrado com sucesso!\n", lista[i].pedido);
-            i++;
-        }
-        else if(atendimento==2){
-
-        }
-         else if(atendimento==3){
-         for(int j= 0;j<=i;j++){
-            printf("Pedido %d: %s - %s - %s\n",
-       Lista[j].pedido, Lista[j].cliente, Lista[j].sabor, Lista[j].tamanho);
-
-         }
-        }
-         else if(atendimento==4){
-            printf("\nProgama encerrado");
-        }
-        else{
-                printf("\nNumero incorreto tente novamente ");
-            }
-    }while (atendimento!= 4);
-    return 0;
-}
 void adicionarNome(char nome[30]) {
     printf("Digite o nome: ");
     getchar();
@@ -71,6 +34,52 @@ void selecionarTamanho(char tamanho[10]) {
         default: strcpy(tamanho, "Invalido"); break;
     }
 }
+
+int main (){
+    int  atendimento;
+    int i =0;
+    int final =0;
+    int l;
+    for(l =0;l<=199;l++){
+        Lista[l].atendido=0;
+    }
+    do{
+        printf("\n[1] Novo pedido\n[2] Atender Pedido\n[3] Ver fila\n[4]Encerrar progama\n");
+        scanf("%d",&atendimento);
+        if(atendimento == 1){
+          printf("\nNovo Pedido\n");
+            Lista[i].pedido = i + 1;
+            adicionarNome(Lista[i].cliente);
+            adicionarSabor(Lista[i].sabor);
+            selecionarTamanho(Lista[i].tamanho);
+            printf("Pedido %d registrado com sucesso!\n", Lista[i].pedido);
+            i++;
+        }
+        else if(atendimento==2){
+
+        }
+         else if(atendimento==3){
+         for(int j= 0;j<=i;j++){
+            if(i==0){
+                printf("\nNao ha pedidos.\n");
+            }
+            if(Lista[j].atendido){
+                printf("\nPedido\t %d:\t %s  \t%s  \t%s\n",
+                Lista[j].pedido, Lista[j].cliente, Lista[j].sabor, Lista[j].tamanho);
+            }
+
+         }
+        }
+         else if(atendimento==4){
+            printf("\nProgama encerrado");
+        }
+        else{
+                printf("\nNumero incorreto tente novamente ");
+            }
+    }while (atendimento!= 4);
+    return 0;
+}
+
 
 
 
